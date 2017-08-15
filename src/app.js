8296551e9fc811505969bdc8ab421a3e40ad92f0
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-15 14:01:51 -0700

    add animiation

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 //
 import Button from './components/Button';
 import Jumbotron from './components/Jumbotron';
 
 import './sass/app.scss';
 
-export default ()=>{
-  return(
-    <Jumbotron className = '-fluid custom'>
-      <h1> Build React.js UI </h1>   
-      <p> with Bootstrap and SASS </p>
-      <p>
-        <Button className="-primary" href="www.google.com" target="_blank"></Button>
-        <Button className="-danger-outline -sm" disabled></Button>
-      </p>
-      <Button className="-primary-outline -lg -block" disabled/>
-    </Jumbotron>
+export default () => {
+  return (
+    <ReactCSSTransitionGroup transitionName='app' transitionAppear={true}
+      transitionAppearTimeout={500}>
+      <Jumbotron className='-fluid custom' containerFluid={true}>
+        <h1> Build React.js UI </h1>
+        <p> with Bootstrap and SASS </p>
+        <p>
+          <Button className="-primary" href="www.google.com" target="_blank"></Button>
+          <Button className="-danger-outline -sm" disabled></Button>
+        </p>
+        <Button className="-primary-outline -lg -block" disabled />
+      </Jumbotron>
+    </ReactCSSTransitionGroup>
   );
 }
